560b9f671761c147b7ad338384ccac5297ee63ab
karen-mizo <ktaisaarm_3531@icloud.com>
2020-04-11 19:59:38 +0900

    automatic-uodating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,9 @@
 $(function(){ 
+
   function buildHTML(message){
    if ( message.image ) {
      var html =
-        `<div class="chat_message__post">
+        `<div class="chat_message__post" data-message-id=${message.id}>
           <div class="chat_message__post__user">
             <div class="chat_message__post__user-name">
               ${message.user_name}
@@ -21,7 +22,7 @@ $(function(){
      return html;
     } else {
      var html =`
-        <div class="chat_message__post">
+        <div class="chat_message__post" data-message-id=${message.id}>
           <div class="chat_message__post__user">
             <div class="chat_message__post__user-name">
               ${message.user_name}
@@ -62,4 +63,32 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.chat_message__post:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat_message').append(insertHTML);
+        $('.chat_message').animate({ scrollTop: $('.chat_message')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
+
+  
